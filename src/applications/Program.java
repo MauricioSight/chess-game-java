4841f0d731321cdbfbdf73d8deb7fac117e30bd8
Mauricio Sightman <mauriciosightman@gmail.com>
2019-02-26 17:38:28 -0300

    First class: Position

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,10 +1,14 @@
 package applications;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		
+		Position pos = new Position(5, 3);
+		System.out.println(pos);
 	}
 
 }
