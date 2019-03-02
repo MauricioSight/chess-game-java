e9c916ec481b8dd53348d52376bbb770e5be303f
Mauricio Sightman <mauriciosightman@gmail.com>
2019-03-02 00:19:56 -0300

    Printing possible moves

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,11 +1,12 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-	
+
 	public King(Board board, Color color) {
 		super(board, color);
 	}
