e728ece2c2d4299b1451c3a874165daf5fcac270
Mauricio Sightman <mauriciosightman@gmail.com>
2019-03-08 18:43:11 -0300

    Check logic

--- src/applications/UI.java
+++ src/applications/UI.java
@@ -70,7 +70,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waitng player: " + chessMatch.getCurrentPlayer());
-		
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
