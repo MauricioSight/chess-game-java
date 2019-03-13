d047926d59e8e5903af46e7d32fe436fe7741d4d
Mauricio Sightman <mauriciosightman@gmail.com>
2019-03-13 15:57:49 -0300

    Special move - Promotion

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,5 +1,6 @@
 package applications;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -13,11 +14,11 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -25,31 +26,52 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-			}
-			catch (ChessException e) {
+
+				if (chessMatch.getPromoted() != null) {
+					boolean error = true;
+					while (error) {
+						try {
+							UI.clearScreen();
+							UI.printBoard(chessMatch.getPieces(), possibleMoves);
+							System.out.print("Target: " + target);
+							
+							System.out.println();
+							System.out.print("Enter piece for prmotion (B/N/R/Q): ");
+							String type = sc.nextLine();
+							chessMatch.replacePromotedPiece(type);
+							error = false;
+
+						} catch (ChessException e) {
+							System.out.println(e.getMessage());
+							sc.nextLine();
+							error = true;
+						}
+					}
+				}
+
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
-			catch (InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 		}
-		 UI.clearScreen();
-		 UI.printMatch(chessMatch, captured);
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
